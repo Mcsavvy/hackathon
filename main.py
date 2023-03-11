a62bbeffbd4a167095d3d8636378add0811ede54
mcsavvy <phishink5@gmail.com>
2023-03-11 01:48:37 +0100

    Setting Up

--- main.py
+++
@@ -1,6 +0,0 @@
-from qdrant_client import QdrantClient
-
-qdrant_client = QdrantClient(
-    host="62e0a08a-a569-42f5-a18a-88027a2215da.us-east-1-0.aws.cloud.qdrant.io", 
-    api_key="3AvOHVV1QJH-PoUEqXgb2ydUZJQzK6P_wQlA8tqKK30GkmyTdmNlqg",
-)
