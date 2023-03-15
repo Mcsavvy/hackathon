b931c8a64b50dcb3c981f4ee0f6394bbeb5f94dc
Afam Ugwuanyi <phyrokelstein2@gmail.com>
2023-03-15 19:23:59 +0000

    Created a wrapper function for the neural_searcherand api

---
+++ __init__.py
@@ -0,0 +1,2 @@
+from backend.neural_search import NeuralSearcher
+search_bar = NeuralSearcher(collection_name)
--- api.py
+++ api.py
@@ -1,12 +1,32 @@
+"""This module contains two major functions, our run funtion which
+is basically a wrapper function for our searcher and a fast api
+which broadcasts our information."""
 from fastapi import FastAPI
 import uvicorn
+from backend.neural_search import NeuralSearcher
+from backend.generate_response import interpret
+import search_bar
+
 
 def run(query):
-    return f"Query successfully received {query}"
+    """This takes a query and searches it with the aid of our neural searcher
+It then converts the information into a user friendly message useing our interpret
+function and then returns a json response consisting of image and description pairs"""
+    results = search_bar.search(query)
+    response = []
+    for result in results:
+        temp_dict = {}
+        message = interpret(result)
+        temp_dict.update(description=message)
+        temp_dict.update(images=result['img'])
+        response.append(result)
+    return response
+
+
 salesman = FastAPI()
-books = {1: "yes", 2: "no"}
 @salesman.get('/salesman/{query}')
 async def perfect_salesman():
-    return {"recommendation": run(query)}
+    return {"recommendations": run(query)}
+
 if __name__ == '__main__':
     uvicorn.run(salesman, host='127.0.0.1', port='8000')
